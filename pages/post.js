371e9118b0f574eeb03beef26cdde64f69139bec
Wesley Bonneville <westazbonneville@gmail.com>
2020-02-11 11:20:45 -0500

    Update post.js

--- pages/post.js
+++ pages/post.js
@@ -2,10 +2,13 @@ import { useRouter } from "next/router";
 import Layout from "../components/MyLayout";
 
 const Page = () => {
+  // return router object
+  // useRouter is a React hook
   const router = useRouter();
 
   return (
     <Layout>
+      {/* use routers query object */}
       <h1>{router.query.title}</h1>
       <p>This is the blog post content.</p>
     </Layout>
